e7c1cc73a6e35d8a43f491e5daeb2ae182b987d3
Sergey UP <sergey10goals@gmail.com>
2024-03-17 00:17:09 +0200

    updated Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,17 @@ const Navbar = () => {
         <img src={appleImg} alt="apple logo" width={14} height={18} />
 
         <ul className="flex flex-1 justify-center max-sm:hidden">
-          {navLists.map((nav) => {
-            <li key={nav} className="">
+          {navLists.map((nav) => (
+            <li
+              key={nav}
+              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-colors"
+            >
               {nav}
-            </li>;
-          })}
+            </li>
+          ))}
         </ul>
 
-        <div>
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
           <img src={searchImg} alt="search icon" width={18} height={18} />
           <img src={bagImg} alt="bag icon" width={18} height={18} />
         </div>
