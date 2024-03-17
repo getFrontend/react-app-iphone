317afa4bd5ca14067c9bbbef380e89199f92dc37
Sergey UP <sergey10goals@gmail.com>
2024-03-17 22:04:22 +0200

    updated: added styles to main section

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <main className="">
+    <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
