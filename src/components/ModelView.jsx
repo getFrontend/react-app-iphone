1005492b6fce4d557d847cac801020eb03b98c49
Sergey UP <sergey10goals@gmail.com>
2024-03-19 10:39:40 +0200

    Implemented 3D Model view

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,6 +1,9 @@
-import { PerspectiveCamera, View } from "@react-three/drei";
-import Lights from "./Lights";
 import { Suspense } from "react";
+import * as THREE from "three";
+import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei";
+import Lights from "./Lights";
+import IPhone from "./IPhone";
+import Loader from "./Loader";
 
 const ModelView = ({
   index,
@@ -24,7 +27,29 @@ const ModelView = ({
 
       <Lights />
 
-      <Suspense fallback={<div>Loading...</div>}></Suspense>
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? 'small' : 'large`}
+        position={[0, 0, 0]}
+      >
+        <Suspense fallback={<Loader />}>
+          <IPhone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
     </View>
   );
 };
