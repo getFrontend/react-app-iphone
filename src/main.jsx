866aca062672761d70f7dba6c41d8b9bb5dae60f
Sergey UP <sergey10goals@gmail.com>
2024-03-19 12:07:57 +0200

    added Sentry to monitor app perfomance in real time

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://52481e50e86575276dfb57db08afcf8e@o4506936781832192.ingest.us.sentry.io/4506936788516864",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
