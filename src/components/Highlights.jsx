5fddcd83a0d558e6d3ed7c11c7de523fb76ab8e9
Sergey UP <sergey10goals@gmail.com>
2024-03-19 14:03:26 +0200

    fixed: scroll trigger animation

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,11 +2,12 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { rightImg, watchImg } from "../utils";
 import VideoCarousel from "./VideoCarousel";
+import { animateWithGsap } from "../utils/animations";
 
 const Highlights = () => {
   useGSAP(() => {
-    gsap.to("#title", { opacity: 1, y: 0 });
-    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
+    animateWithGsap("#title", { opacity: 1, y: 0 });
+    animateWithGsap(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
   }, []);
 
   return (
