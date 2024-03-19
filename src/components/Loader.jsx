07a5bb85d0f57a48fe0deffdf7bc9ae7317e4ab3
Sergey UP <sergey10goals@gmail.com>
2024-03-19 12:11:53 +0200

    fixed className in Loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,8 +5,8 @@ const Loader = () => {
   return (
     <Html>
       <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-        <span class="sr-only">Loading...</span>
-        <div class="w-16 h-16 border-8 border-dashed rounded-full animate-spin border-gray-100"></div>
+        <span className="sr-only">Loading...</span>
+        <div className="w-16 h-16 border-8 border-dashed rounded-full animate-spin border-gray-100"></div>
       </div>
     </Html>
   );
